859edc3b31f5ef916aa531a0b2017fbb51b22e87
wlcross <william589@revature.net>
2022-08-26 11:15:06 -0400

    Initial Commit

---
+++ src/main/java/dev/cross/bumble2/Bumble2Application.java
@@ -0,0 +1,13 @@
+package dev.cross.bumble2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bumble2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bumble2Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/cross/bumble2/Bumble2ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cross.bumble2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bumble2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
