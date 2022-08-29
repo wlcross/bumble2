4dc0182edc4c4c390144f53113171c25875109d8
wlcross <william589@revature.net>
2022-08-29 18:49:32 -0400

    Register Navigation

--- frontend/bumble2/src/app/app.module.ts
+++ frontend/bumble2/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { BasicSetupComponent } from './basic-setup/basic-setup.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { BasicSetupComponent } from './basic-setup/basic-setup.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/bumble2/src/app/login/login.component.ts
+++ frontend/bumble2/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  username ="";
+  pass = "";
+
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  login() {
+    //Insert Login Validation
+  }
+
+  register() {
+    this.router.navigate(['register']);
   }
 
 }
