586b04939c03d83a4542d01c8499c133a8f1f705
wlcross <william589@revature.net>
2022-08-29 15:32:14 -0400

    Added some basic angular routing

--- frontend/bumble2/src/app/app.module.ts
+++ frontend/bumble2/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { BasicSetupComponent } from './basic-setup/basic-setup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    BasicSetupComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/bumble2/src/app/basic-setup/basic-setup.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BasicSetupComponent } from './basic-setup.component';
+
+describe('BasicSetupComponent', () => {
+  let component: BasicSetupComponent;
+  let fixture: ComponentFixture<BasicSetupComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BasicSetupComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(BasicSetupComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
