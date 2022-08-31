79fb92f2d5339f94911d013f11ee1a7a119326b3
wlcross <william589@revature.net>
2022-08-31 10:34:03 -0400

    Allowed CrossOrigin for Spring

--- backend/src/main/java/dev/cross/controller/AccountController.java
+++ backend/src/main/java/dev/cross/controller/AccountController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,7 @@ import dev.cross.model.BasicInfo;
 import dev.cross.model.Login;
 import dev.cross.service.AccountService;
 
-
+@CrossOrigin
 @RestController
 public class AccountController {
 	
